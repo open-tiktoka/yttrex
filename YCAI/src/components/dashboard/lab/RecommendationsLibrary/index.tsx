80b0aa2efafc4e3f7b326a16126e27ce58ada6c5
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-31 13:02:38 +0100

    feat: create recommendations library page

--- YCAI/src/components/dashboard/lab/RecommendationsLibrary/index.tsx
+++ YCAI/src/components/dashboard/lab/RecommendationsLibrary/index.tsx
@@ -1,38 +1,8 @@
-import { Box, Grid } from '@material-ui/core';
-import { ErrorBox } from '@shared/components/Error/ErrorBox';
-import * as QR from 'avenger/lib/QueryResult';
-import { WithQueries } from 'avenger/lib/react';
-import { LazyFullSizeLoader } from '../../../common/FullSizeLoader';
 import * as React from 'react';
-import { addRecommendation } from '../../../../state/dashboard/creator.commands';
-import * as queries from '../../../../state/dashboard/creator.queries';
-import AddRecommendationBox from '../AddRecommendationBox';
-import RecommendationList from './RecommendationsList';
+import { Box } from '@material-ui/core';
 
 const RecommendationsLibrary: React.FC = () => {
-  const handleRecommendationAdd = React.useCallback((url: string) => {
-    void addRecommendation({
-      url,
-    })();
-  }, []);
-
-  return (
-    <WithQueries
-      queries={{ recommendations: queries.creatorRecommendations }}
-      render={QR.fold(LazyFullSizeLoader, ErrorBox, ({ recommendations }) => {
-        return (
-          <Grid container>
-            <Grid item md={6}>
-              <AddRecommendationBox onAddClick={handleRecommendationAdd} />
-              <Box>
-                <RecommendationList recommendations={recommendations} />
-              </Box>
-            </Grid>
-          </Grid>
-        );
-      })}
-    />
-  );
+  return <Box>Recommendations Library</Box>;
 };
 
 export default RecommendationsLibrary;
